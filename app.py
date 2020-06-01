2353dcb32ad0f3d5826f6795c467ca1b5b790a79
Primadonnabit <ai55@psrmedia.kr>
2020-06-01 16:25:04 +0900

    테스트

--- app.py
+++ app.py
@@ -16,5 +16,9 @@ configure_uploads(app, photos)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000, debug=True)
